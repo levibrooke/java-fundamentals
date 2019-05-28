89f2aab7d981f19af4bcc35a99718fa5fec290fa
Levi Porter <levi@levibrooke.com>
2019-05-28 11:23:45 -0700

    Fix main method declaration

--- basics/Main.java
+++ basics/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-  public static void main() {
+  public static void main(String[] args) {
     int dogCount = 1;
     System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
 
