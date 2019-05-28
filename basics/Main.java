3695843e8646f2abc858e3eeac3167bbee83d170
Levi Porter <levi@levibrooke.com>
2019-05-28 11:21:36 -0700

    Create Main.java, write pluralize method

---
+++ basics/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main() {
+    int dogCount = 1;
+    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+    int catCount = 2;
+    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+    int turtleCount = 0;
+    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+  }
+
+  public static String pluralize(String word, int num) {
+    if (num == 1) {
+      return word;
+    }
+    return word + "s";
+  }
+}
