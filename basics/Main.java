9e9ee108ba2cc3095a28c7f1073eeff8fbb8f3bb
Levi Porter <levi@levibrooke.com>
2019-05-28 11:44:15 -0700

    Add flipping coins method

--- basics/Main.java
+++ basics/Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
+    // Pluralize
     int dogCount = 1;
     System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
 
@@ -8,6 +9,10 @@ public class Main {
 
     int turtleCount = 0;
     System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+    // Flipping Coins
+    flipNHeads(1);
+    flipNHeads(5);
   }
 
   public static String pluralize(String word, int num) {
@@ -15,5 +20,23 @@ public class Main {
       return word;
     }
     return word + "s";
+  }
+
+  public static void flipNHeads(int n) {
+    int headsCount = 0;
+    int flips = 0;
+
+    while (headsCount < n) {
+      flips++;
+      if (Math.random() < 0.5) {
+        headsCount = 0;
+        System.out.println("tails");
+      }
+      else if (Math.random() >= 0.5) {
+        headsCount++;
+        System.out.println("heads");
+      }
+    }
+    System.out.println("It took " + flips + " flips to flip " + n + " heads in a row.");
   }
 }
